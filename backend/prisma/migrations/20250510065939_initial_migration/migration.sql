cf5d5b077a050b418058697cf65cff5a71d8de4a
Jann <jann7x@gmail.com>
2025-05-11 03:37:10 +0900

    removed drop tables

--- backend/prisma/migrations/20250510065939_initial_migration/migration.sql
+++ backend/prisma/migrations/20250510065939_initial_migration/migration.sql
@@ -1,9 +1,3 @@
--- TODO: remove this after prod deploy
-DROP TABLE `AllowedHosts`;
-
--- TODO: remove this after prod deploy
-DROP TABLE `User`;
-
 -- CreateTable
 CREATE TABLE `user` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
